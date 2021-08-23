41c4ea95bfc39007e45c454ce2dfc62fd58cb42f
Yug34 <vertilax1337@gmail.com>
2021-08-24 00:08:11 +0530

    Setup background

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from 'react'
+import WAVES from 'vanta/dist/vanta.waves.min'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    let [vantaRef] = useState(React.createRef());
+
+    useEffect(() => {
+        // shininess: 57.00,
+        let vantaEffect = WAVES({
+            el: vantaRef.current,
+            mouseControls: true,
+            touchControls: true,
+            gyroControls: false,
+            minHeight: 200.00,
+            minWidth: 200.00,
+            scale: 2.0,
+            waveHeight: 11.50,
+            waveSpeed: 0.70,
+            scaleMobile: 1.00,
+            zoom: 1.32,
+            color: 0x0
+        });
+    }, [vantaRef])
+
+    return (
+        <div className="App">
+            <div className="App-header" ref={vantaRef}>
+            </div>
+        </div>
+    );
 }
 
 export default App;
