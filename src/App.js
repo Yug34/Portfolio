fd8c25210e1c92dae500b8ffe4f8f935099f8ba3
Yug34 <vertilax1337@gmail.com>
2021-08-24 00:48:49 +0530

    Added carousel

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import './App.css';
 import React, {useState, useEffect} from 'react'
 import WAVES from 'vanta/dist/vanta.waves.min'
+import Typed from 'typed.js';
+import {Carousel} from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 function App() {
     let [vantaRef] = useState(React.createRef());
 
     useEffect(() => {
+        // eslint-disable-next-line no-unused-vars
+        let typed = new Typed('#title', {
+            strings: ['Software Engineer', 'Research Assistant'],
+            typeSpeed: 70,
+            // backSpeed: 35,
+            // backDelay: 1000,
+            // smartBackspace: true,
+            loop: true,
+            loopCount: Infinity,
+            showCursor: false,
+            fadeOut: true
+        });
+    }, [])
+
+    useEffect(() => {
         // shininess: 57.00,
+        // eslint-disable-next-line no-unused-vars
         let vantaEffect = WAVES({
             el: vantaRef.current,
             mouseControls: true,
@@ -26,6 +45,17 @@ function App() {
     return (
         <div className="App">
             <div className="App-header" ref={vantaRef}>
+                <Carousel
+                    showArrows={true}
+                >
+                    <div id="nameTitle">
+                        <div id="name">Yug</div>
+                        <div id="title"/>
+                    </div>
+                    <div>
+                        <div className="sec" style={{height: "100vh", width:"100vw"}}>Hey!</div>
+                    </div>
+                </Carousel>
             </div>
         </div>
     );
