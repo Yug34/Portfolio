cff7b0bef1f67277815e9b929b65663ba5f5e871
Yug34 <vertilax1337@gmail.com>
2021-08-31 00:48:59 +0530

    App refactor, broke down into components

--- src/App.js
+++ src/App.js
@@ -4,20 +4,12 @@ import WAVES from "vanta/dist/vanta.waves.min";
 import Typed from "typed.js";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import Tilt from "react-parallax-tilt";
 
-import ReactCardFlip from "react-card-flip";
-
-import discordImg from "./images/discord.png";
+import TiltContainer from "./Components/TiltContainer";
+import TypedName from "./Components/TypedName";
 
 function App() {
   let [vantaRef] = useState(React.createRef());
-  let [isFlipped, setIsFlipped] = useState(false);
-
-  const handleClick = (e) => {
-    e.preventDefault();
-    setIsFlipped(!isFlipped);
-  };
 
   useEffect(() => {
     // eslint-disable-next-line no-unused-vars
@@ -62,77 +54,9 @@ function App() {
           statusFormatter={() => {}}
           useKeyboardArrows={true}
         >
-          <div id="nameTitle">
-            <div id="name">
-              <b>Yug</b>
-            </div>
-            <div id="title" />
-          </div>
+          <TypedName />
 
-          <div
-            className="tiltContainer"
-            style={{ height: "100vh", width: "100vw" }}
-          >
-            <ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal">
-              <Tilt
-                className={isFlipped ? "nullbox" : "box"}
-                id="box1"
-                glareEnable={true}
-                glareMaxOpacity={0.5}
-                glareColor="#ffffff"
-                glarePosition="bottom"
-              >
-                <h2 className="name">AllBOT</h2>
-                <a
-                  id="allbotMoreInfo"
-                  href="https://github.com/Yug34/allbot"
-                  className="buy"
-                  onClick={(e) => {
-                    handleClick(e);
-                  }}
-                >
-                  More Info
-                </a>
-                <div className="circle" />
-                <img src={discordImg} className="product" alt="discordImg" />
-              </Tilt>
-              <Tilt
-                className={isFlipped ? "box" : "nullbox"}
-                id="box2"
-                glareEnable={true}
-                glareMaxOpacity={0.5}
-                glareColor="#ffffff"
-                glarePosition="bottom"
-              >
-                <ul className="list">
-                  <li>
-                    Discord bot written in <i>Node.js</i>
-                  </li>
-                  <li>Stream music from YouTube</li>
-                  <li>Currency system and a shop</li>
-                  <li>Snarky.</li>
-                  <li>Kanye quotes if you need them</li>
-                  <li>Aren't you sold already?</li>
-                </ul>
-                <p
-                  id="allbotUnflip"
-                  className="buy"
-                  onClick={(e) => {
-                    handleClick(e);
-                  }}
-                >
-                  Unflip
-                </p>
-                <a
-                  id="allbotSource"
-                  href="https://github.com/Yug34/allbot"
-                  className="sell"
-                >
-                  View Source
-                </a>
-              </Tilt>
-            </ReactCardFlip>
-          </div>
+          <TiltContainer />
         </Carousel>
       </div>
     </div>
---
+++ src/Components/TypedName.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function TypedName() {
+  return (
+    <div id="nameTitle">
+      <div id="name">
+        <b>Yug</b>
+      </div>
+      <div id="title" />
+    </div>
+  );
+}
+
+export default TypedName;
