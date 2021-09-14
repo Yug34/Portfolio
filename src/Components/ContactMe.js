b88c91d9a8723d3e12bdc12775eb2b952546a763
Yug34 <vertilax1337@gmail.com>
2021-09-14 10:56:01 +0530

    Disabled form auto completion, minor style changes

--- src/Components/ContactMe.js
+++ src/Components/ContactMe.js
@@ -17,38 +17,60 @@ function ContactMe() {
       <form onSubmit={handleSubmit}>
         Contact me!
         <div className="inputBlock" id="emailBlock">
-          <label className="inputLabel" id="emailLabel" htmlFor="email">
-            Email Address
-          </label>
-          <input
-            className="formInput"
-            id="email"
-            type="email"
-            name="email"
-            autoComplete={false}
-          />
-          <label className="inputLabel" id="subjectLabel" htmlFor="subject">
-            Email Address
-          </label>
-          <input
-            className="formInput"
-            id="subject"
-            type="text"
-            name="subject"
-            autoComplete={false}
-          />
+          <div className="inlineEmail">
+            <label className="inputLabel" id="emailLabel" htmlFor="email">
+              Email Address:
+            </label>
+            <input
+              className="formInput"
+              id="email"
+              type="email"
+              name="email"
+              autoComplete="none"
+            />
+          </div>
+          <div className="inlineSubject">
+            <label className="inputLabel" id="subjectLabel" htmlFor="subject">
+              Subject:
+            </label>
+            <input
+              className="formInput"
+              id="subject"
+              type="text"
+              name="subject"
+              autoComplete="none"
+            />
+          </div>
         </div>
         <ValidationError prefix="Email" field="email" errors={state.errors} />
         <div className="inputBlock" id="messageBlock">
           <label className="inputLabel" id="messageLabel" htmlFor="message">
             Message
           </label>
-          <textarea className="formInput" id="message" name="message" />
+          <textarea
+            className="formInput"
+            id="message"
+            name="message"
+            autoComplete="none"
+          />
         </div>
         <ValidationError
           prefix="Message"
           field="message"
           errors={state.errors}
+        />
+        {/*Do not remove, You need at least one valid input field with autoComplete="on"*/}
+        <input
+          type="text"
+          autoComplete="on"
+          value=""
+          style={{
+            display: "none",
+            opacity: 0,
+            position: "absolute",
+            left: "-100000px",
+          }}
+          readOnly={true}
         />
         <button type="submit" disabled={state.submitting}>
           Submit
