749b19c17f90a4a607bb3d8c46280ccd4a5fc347
Yug34 <vertilax1337@gmail.com>
2021-09-18 22:51:36 +0530

    Contact page style cleanup, moved to inline border styles

--- src/Components/ContactMe.js
+++ src/Components/ContactMe.js
@@ -7,8 +7,10 @@ function ContactMe() {
 
   if (state.succeeded) {
     return (
-      <div className="formBox-success">
-        <p>Thanks for contacting me!</p>
+      <div className="formBox">
+        <div style={{ border: "1px solid greenyellow" }}>
+          <p>Thanks for contacting me! You're awesome. :)</p>
+        </div>
       </div>
     );
   }
@@ -19,80 +21,82 @@ function ContactMe() {
 
   return (
     <div className="formBox">
-      <form onSubmit={handleSubmit}>
-        Contact me!
-        <div className="inputBlock" id="emailBlock">
-          <div className="inlineEmail">
-            <label className="inputLabel" id="emailLabel" htmlFor="email">
-              Email Address:
+      <div style={{ border: "1px solid white" }}>
+        <form onSubmit={handleSubmit}>
+          Contact me!
+          <div className="inputBlock" id="emailBlock">
+            <div className="inlineEmail">
+              <label className="inputLabel" id="emailLabel" htmlFor="email">
+                Email Address:
+              </label>
+              <input
+                className="formInput"
+                id="email"
+                type="email"
+                name="email"
+                autoComplete="none"
+              />
+            </div>
+            <div className="inlineSubject">
+              <label className="inputLabel" id="subjectLabel" htmlFor="subject">
+                Subject:
+              </label>
+              <input
+                name="subject"
+                className="formInput"
+                id="subject"
+                type="text"
+                autoComplete="none"
+                value={value}
+                onChange={handleChange}
+              />
+            </div>
+          </div>
+          <ValidationError prefix="Email" field="email" errors={state.errors} />
+          <div className="inputBlock" id="messageBlock">
+            <label className="inputLabel" id="messageLabel" htmlFor="message">
+              Message:
             </label>
-            <input
+            <textarea
               className="formInput"
-              id="email"
-              type="email"
-              name="email"
+              id="message"
+              name="message"
+              autoComplete="none"
+            />
+            <label className="inputLabel" id="sideNoteLabel" htmlFor="sideNote">
+              An optional but highly recommended positive message to me pretty
+              please?
+            </label>
+            <textarea
+              className="formInput"
+              id="sideNote"
+              name="sideNote"
               autoComplete="none"
             />
           </div>
-          <div className="inlineSubject">
-            <label className="inputLabel" id="subjectLabel" htmlFor="subject">
-              Subject:
-            </label>
-            <input
-              name="subject"
-              className="formInput"
-              id="subject"
-              type="text"
-              autoComplete="none"
-              value={value}
-              onChange={handleChange}
-            />
-          </div>
-        </div>
-        <ValidationError prefix="Email" field="email" errors={state.errors} />
-        <div className="inputBlock" id="messageBlock">
-          <label className="inputLabel" id="messageLabel" htmlFor="message">
-            Message:
-          </label>
-          <textarea
-            className="formInput"
-            id="message"
-            name="message"
-            autoComplete="none"
+          <ValidationError
+            prefix="Message"
+            field="message"
+            errors={state.errors}
           />
-          <label className="inputLabel" id="sideNoteLabel" htmlFor="sideNote">
-            An optional but highly recommended positive message to me pretty
-            please?
-          </label>
-          <textarea
-            className="formInput"
-            id="sideNote"
-            name="sideNote"
-            autoComplete="none"
+          {/*Do not remove, You need at least one valid input field with autoComplete="on"*/}
+          <input
+            type="text"
+            autoComplete="on"
+            value=""
+            style={{
+              display: "none",
+              opacity: 0,
+              position: "absolute",
+              left: "-100000px",
+            }}
+            readOnly={true}
           />
-        </div>
-        <ValidationError
-          prefix="Message"
-          field="message"
-          errors={state.errors}
-        />
-        {/*Do not remove, You need at least one valid input field with autoComplete="on"*/}
-        <input
-          type="text"
-          autoComplete="on"
-          value=""
-          style={{
-            display: "none",
-            opacity: 0,
-            position: "absolute",
-            left: "-100000px",
-          }}
-          readOnly={true}
-        />
-        <button id="submitEmail" type="submit" disabled={state.submitting}>
-          Submit
-        </button>
-      </form>
+          <button id="submitEmail" type="submit" disabled={state.submitting}>
+            Submit
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
